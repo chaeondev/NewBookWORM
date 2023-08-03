7c63b3ac6575d78144c181135d4e635f06071013
chaeondev <ycw0363@gmail.com>
2023-08-04 02:52:17 +0900

    design Main Collection View

--- BookWORM/BookCollectionViewCell.swift
+++ BookWORM/BookCollectionViewCell.swift
@@ -9,9 +9,51 @@ import UIKit
 
 class BookCollectionViewCell: UICollectionViewCell {
 
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var posterImageView: UIImageView!
+    @IBOutlet var rateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        setCell()
+        setNameLabel()
+        setRateLabel()
+        
     }
 
+    func configureCell(row: Movie) {
+        nameLabel.text = row.title
+        posterImageView.image = UIImage(named: row.title)
+        rateLabel.text = "★ \(row.rate)"
+        
+        let like = row.like ? UIImage(systemName: "heart.fill") : UIImage(systemName: "heart")
+        likeButton.setImage(like, for: .normal)
+        
+        self.backgroundColor = row.color
+    }
+    
+    func setCell() {
+        self.layer.cornerRadius = 10
+    }
+    
+    func setNameLabel() {
+        nameLabel.numberOfLines = 0
+        nameLabel.textColor = .white
+        nameLabel.font = .systemFont(ofSize: 20, weight: .heavy)
+        nameLabel.textAlignment = .right
+    }
+    
+    
+    func setRateLabel() {
+        rateLabel.textColor = .white
+        rateLabel.font = .systemFont(ofSize: 15, weight: .semibold)
+    }
+    
+    
+    
 }
--- BookWORM/BookWormCollectionViewController.swift
+++ BookWORM/BookWormCollectionViewController.swift
@@ -20,9 +20,52 @@ class BookWormCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let nib = UINib(nibName: "BookCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "BookCollectionViewCell")
+        
+        setCollectionViewLayout()
         
     }
 
+    func setCollectionViewLayout() {
+        let layout = UICollectionViewFlowLayout()
+        
+        let spacing:CGFloat = 16
+        let width = UIScreen.main.bounds.width - (spacing * 3)
+        
+        guard let tabBarHeight = self.tabBarController?.tabBar.frame.height else { return }
+        guard let naviHeight = self.navigationController?.navigationBar.frame.height else { return }
+        let height = UIScreen.main.bounds.height - tabBarHeight - naviHeight - (spacing * 4)
+        
+        layout.itemSize = CGSize(width: width / 2, height: height / 3)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        
+        collectionView.collectionViewLayout = layout
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movie.list.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BookCollectionViewCell", for: indexPath) as? BookCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let row = movie.list[indexPath.row]
+        
+        cell.configureCell(row: row)
+        
+        cell.likeButton.tag = indexPath.row
+        cell.likeButton.addTarget(self, action: #selector(likeButtonClicked), for: .touchUpInside)
+        
+        return cell
+    }
+    
+    @objc func likeButtonClicked(_ sender: UIButton) {
+        movie.list[sender.tag].like.toggle()
+    }
 
 }
